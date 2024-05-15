6b6e9829efd2b74457a0b460de71fec57e52ab93
MonalBarse <monal.barse.cer22@itbhu.ac.in>
2024-05-16 00:15:33 +0530

    Added instructions to use this config

--- README.md
+++ README.md
@@ -2,7 +2,43 @@
 
 Welcome to my Neovim configuration repository! This repository contains my personal Neovim configuration, including settings, key mappings, plugins, and themes tailored to my preferences and workflow.
 
-- I am using ```Linux Ubuntu 22.04.4 LTS 6.5.0-27-generic``` and ```xterm-256color```
+- I am using `Linux Ubuntu 22.04.4 LTS 6.5.0-27-generic` and `xterm-256color`
+
+## Prerequisites
+
+    - Neovim 0.6.0 or later
+    - Ripgrep (rg) 11.0.2 or later
+    - Lazygit 0.30.3 or later
+    ```bash
+        brew install jesseduffield/lazygit/lazygit
+    ```
 
 ## Steps to use my config
 
+1. Create a backup of your existing Neovim configuration (if you have one) by running the following command:
+   ```bash
+   mv ~/.config/nvim ~/.config/nvim.bak
+   ```
+2. Create a new Neovim configuration directory by running the following command:
+   ```bash
+   mkdir -p ~/.config/nvim
+   ```
+3. Clone this repository into the newly created Neovim configuration directory by running the following command:
+   `bash
+    git clone
+    `
+   and remove .git folder
+   `bash
+    rm -rf .git
+    `
+   (NOTE: I have used my name as the directory inside the lua folder, feel free to change it to your name or any other name you like. Just make sure to change `monal` in every file to the name you choose.)
+
+4. Install the plugins by opening Neovim and running the following command:
+
+   ```vim
+   :Lazy
+   ```
+
+   and click I and U to install and update plugins
+
+5. Restart Neovim to apply the changes.
--- lua/monal/plugins/alpha.lua
+++ lua/monal/plugins/alpha.lua
@@ -1,38 +1,53 @@
 return {
-  "goolord/alpha-nvim",
-  event = "VimEnter",
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-    -- Set header
-    dashboard.section.header.val = {
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
-    }
+		-- Set header
+		dashboard.section.header.val = {
+			[[                                 ░▒▒▒▒▒▒▒▒▒▒░                ░     ░          ]],
+			[[      ░                  ▒▒░                   ░ ▒▒          ░      ░         ]],
+			[[   ░    ░            ▒▒                             ▒▓        ░     ░         ]],
+			[[  ░               ▒▒                                  ░▓      ░    ░          ]],
+			[[  ░     ░       ▒░                ░     ░  ░░ ░░░░░░    ░▓    ░    ░          ]],
+			[[   ░          ▒▒               ░▒▒▒▒▒░▒▒░       ▒▒ ░▒▒░   ▒▒ ░▓█▒    ▒        ]],
+			[[    ░      ░░░░  ░          ░▒▒░             ░▓▒       ░▒   ░░ ▒▒     ▒       ]],
+			[[     ░      ░  ░▒░        ░▒▒▒▓▓▓▓▒▒            ░▒██████▒▒  ░▒▒▒▒░    ░       ]],
+			[[      ░      ░░░         ▒░   ░░              ░▒░░░▒▓▒░░░ ░ ░▒▓▒▒▒    ▒       ]],
+			[[       ░      ░        ░▒░▒▓██▓▓▓▒░░            ░ ███      ░░░░▓▓▒░   ▒       ]],
+			[[        ░      ░░░░    ░▒█▓▒░▒███▓░               ███▒ ░▒   ░ ░▒░  ▒▒▒▓       ]],
+			[[       ░ ░     ▒  ░▒░░░▒░░░░░███▓                 ▒▓░██░▓    ░▒▒▒   ░░░▓      ]],
+			[[       ▓ ░▒▒░▒▒░ ▒░▒▒▒▒░ ░░ ░█▓▒██▓▓█░             ▓░  ░▓     ░▒▒▒░  ░▒▒      ]],
+			[[       ░▓▓░  ░▒░░░ ▒▒▒░      ▓▓  ▒▓▒▓▒                       ░▒▒░░▒▓          ]],
+			[[         ▒    ░▒░ ░  ░░       ▓▒  ░                           ░▒▓░ ▒░         ]],
+			[[          ▒     ▒░ ░░ ░░       ░             ░▒▒▓▓▓▓▒         ░▒▒▒░ ▒         ]],
+			[[           ▒░  ░░   ░░░░░                   ▒▓▓▓▓▓▓▓▒         ░▒▒░░ ▒         ]],
+			[[            ▒▓▓▓░    ░ ░ ░                ▒▓▓▓▓▓▓▓▓▓░        ░▒▒▒░░░░         ]],
+			[[            ░▒░░░     ░   ░              ░▓▓▓▓▓▓▓▓▓▓        ░░▒▒▒░ ▒░         ]],
+			[[             ░▒▒░░░    ░                 ░▓▓▓▓▓▓▓▓▓▓       ░▒▒▒▒░  ▒          ]],
+			[[              ░▒▒▒░░   ░     ░            ▒▓▓▓▓▒▒▒▓░      ░░░▒▒░   ▓          ]],
+			[[                ▒▒░░░░ ░▒░     ░            ▒▒▒▒▒▒░     ░▒░▒▒▒░   ░▓          ]],
+			[[                  ▒▒▒▒▒▒▒▒░     ░░░                  ░░▒░▒▒▒░░    ░▓░         ]],
+			[[                    ░▓▓▒▒▒▒░      ░░▒░░░░▒ ░░░░░░▒▒▒▒▒▒▒▓▒▒░░     ░▒▒         ]],
+			[[                       ░▓▒▒▒▒▒░     ░▒▒░░░░ ░░░░░░▒▒▒▒▒█▓▓▒░░░░░  ░░▒▓         ]],
+		}
+		-- Set menu
+		dashboard.section.buttons.val = {
 
-    -- Set menu
-    dashboard.section.buttons.val = {
-      
-      dashboard.button("e", "ᥫ   New File", "<cmd>ene<CR>"),
-      dashboard.button("␠ ee", "ᥫ   Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("␠ ff", "ᥫ   Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("␠ fs", "ᥫ   Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("␠ wr", "ᥫ   Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-      dashboard.button("q", "ᥫ   Quit NVIM", "<cmd>qa<CR>"),
+			dashboard.button("e", "      New File", "<cmd>ene<CR>"),
+			dashboard.button("_ ee", "      Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("_ ff", "      Find File", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("_ fs", "      Find Word", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("_ wr", "      Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+			dashboard.button("q", "      Quit NVIM", "<cmd>qa<CR>"),
+		}
 
-    }
+		-- Send config to alpha
+		alpha.setup(dashboard.opts)
 
-    -- Send config to alpha
-    alpha.setup(dashboard.opts)
-
-    -- Disable folding on alpha buffer
-    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-  end,
+		-- Disable folding on alpha buffer
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+	end,
 }
